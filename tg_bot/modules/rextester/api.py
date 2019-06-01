d6c26f9558569f61c6c144e637ae70565e440222
ilikegames468 <47333814+ilikegames468@users.noreply.github.com>
2019-06-01 23:35:35 +0530

    Update api.py

--- tg_bot/modules/rextester/api.py
+++ tg_bot/modules/rextester/api.py
@@ -1,6 +1,6 @@
 import requests
 
-from haruka.modules.rextester.langs import languages
+from tg_bot.modules.rextester.langs import languages
 
 ### API warper for rextester.com
 ## Author: Nitan Alexandru Marcel.
